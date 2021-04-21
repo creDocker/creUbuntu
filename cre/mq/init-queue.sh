0153f21421c044bb8ceba59be75b6696b6a2a363
Unknown <michael.kahle@yahoo.de>
2021-04-21 20:47:23 +0200

    run template handling in parallel

--- cre/mq/init-queue.sh
+++ cre/mq/init-queue.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-/cre/gen-template.sh /cre/mq/create-queue.sh.tmpl 
-/cre/gen-template.sh /cre/mq/consume-queue.sh.tmpl 
-/cre/gen-template.sh /cre/mq/publish-queue.sh.tmpl 
+/cre/gen-template.sh /cre/mq/create-queue.sh.tmpl &
+/cre/gen-template.sh /cre/mq/consume-queue.sh.tmpl &
+/cre/gen-template.sh /cre/mq/publish-queue.sh.tmpl &
+
+wait
+
+
