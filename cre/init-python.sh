803e67a5024cf50cc72294c7726e76e3ddb1e667
KMicha <michael.kahle@yahoo.de>
2022-01-25 12:57:50 +0100

    add python dir and initializing of requirements.txt

---
+++ cre/init-python.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Creates requirements.txt
+
+if [ ! -f /cre/python/requirements.txt ]; then
+    pip freeze > /cre/python/requirements.txt
+fi
+
+# https://github.com/bndr/pipreqs
+
+pipreqs --mode gt /cre/python/
