6dee83f449a1ddf4dedfe700684dd919764f3a46
KMicha <michael.kahle@yahoo.de>
2022-01-25 13:28:35 +0100

    test of java version

--- cre/ubuntu-test.sh
+++ cre/ubuntu-test.sh
@@ -57,7 +57,21 @@ fi
 
 isInFile=$(cat /cre/versions.txt | grep -cP "Java \t openjdk version \"$JAVA_VERSION")
 isInFile8=$(cat /cre/versions.txt | grep -cP "Java \t openjdk version \"1.$JAVA_VERSION")
-if [ $isInFile -eq 0 ] || [ $isInFile8 -eq 0 ]; then
+
+if [ $isInFile -eq 0 ]; then
+    echo "java not 1"
+else
+    echo "java 1"
+fi
+
+if [ $isInFile8 -eq 0 ]; then
+    echo "java not 2"
+else
+    echo "java 2"
+fi
+
+
+if [ $isInFile -eq 0 ] && [ $isInFile8 -eq 0 ]; then
     echo "[WARNING]: Wrong version of Java installed!"
     exit 1
 fi
