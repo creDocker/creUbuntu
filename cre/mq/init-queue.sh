880c016447074861988f915cfd033cd95dc35fa0
KMicha <michael.kahle@yahoo.de>
2022-02-07 16:12:54 +0100

    init length

--- cre/mq/init-queue.sh
+++ cre/mq/init-queue.sh
@@ -3,6 +3,7 @@
 /cre/gen-template.sh /cre/mq/create-queue.sh.tmpl &
 /cre/gen-template.sh /cre/mq/consume-queue.sh.tmpl &
 /cre/gen-template.sh /cre/mq/publish-queue.sh.tmpl &
+/cre/gen-template.sh /cre/mq/length-queue.sh.tmpl &
 
 wait
 
