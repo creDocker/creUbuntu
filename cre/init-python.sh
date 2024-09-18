1f2dc1b48628dca2fe033ac6ec8e5f966f6b5e43
KMicha <michael.kahle@yahoo.de>
2024-09-18 11:39:23 +0200

    pipreqs test only

--- cre/init-python.sh
+++ cre/init-python.sh
@@ -2,32 +2,35 @@
 
 # Creates requirements.txt
 
-
-
-echo "Repository name: $GITHUB_REPOSITORY"
-
-# 
 if [ ! -f /cre/python/requirements.txt ]; then
     #CURRENT_YEAR=$(date +'%Y')
     VERSION_YEAR=$CRE_VERSION:0:4
     cp cre/versions/requirements_$VERSION_YEAR.txt /cre/python/requirements.txt
-    pip3 freeze --no-cache-dir > /cre/python/requirements.txt
+    #pip3 freeze --no-cache-dir > /cre/python/requirements.txt
     chmod 777 /cre/python/requirements.txt
     # https://github.com/bndr/pipreqs
-    pipreqs --mode gt --force /cre/python/
+    ## pipreqs --mode gt --force /cre/python/
+    ## pipreqs --mode compat --force /cre/python/
+    pip3 install --no-cache-dir --root-user-action ignore -r /cre/python/requirements.txt
     echo "requirements.txt written"
 fi
-
-cat /cre/python/requirements.txt
-
-# Install
-pip3 install --no-cache-dir --root-user-action ignore -r /cre/python/requirements.txt
 
 # Initialize
 if [ -f /cre/python/initialize.sh ]; then
     chmod 777 /cre/python/initialize.sh
     /cre/python/initialize.sh
 fi
+
+# https://github.com/bndr/pipreqs
+# Generate requirements.txt file for any project based on imports
+## pipreqs --mode compat --force /cre/python/
+cat /cre/python/requirements.txt
+# For now: only compare:
+pipreqs --mode compat --diff /cre/python/requirements.diff /cre/python/
+pipreqs --mode compat --print /cre/python/
+
+# Install
+pip3 install --no-cache-dir --root-user-action ignore -r /cre/python/requirements.txt
 
 # Run main.py or app.py or auto.py?
 
