a1dd25dfd8b32e710996eef2eb79e9716cc2bb59
KMicha <michael.kahle@yahoo.de>
2024-12-05 15:03:01 +0100

    run update versions

--- cre/update-versions.sh
+++ cre/update-versions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+sleep 2
 
 VERSION_YEAR=${CRE_VERSION:0:4}
 CURRENT_YEAR=$(date +'%Y')
 COPY_FILES=false
-
 
 if [ ! -z "$1" ]; then
   VERSION_YEAR=$1
@@ -14,20 +14,11 @@ if [ "$VERSION_YEAR" = "$CURRENT_YEAR" ]; then
   COPY_FILES=true
 fi
 
-echo "$(pwd)"
-
-echo "$VERSION_YEAR $CURRENT_YEAR $COPY_FILES"
-cat /cre/versions.txt
-cat /cre/requirements.txt.orig
-
-echo "cut -f2,3 /cre/versions.txt > cre/versions/versions_$VERSION_YEAR.txt"
-
+cut -f2,3 /cre/versions.txt > /cre/versions/versions_$VERSION_YEAR.txt
 
 if [ $COPY_FILES ]; then
-  echo "cp /cre/requirements.txt.orig cre/versions/requirements_$VERSION_YEAR.txt"
+  echo cp /cre/requirements.txt.orig /cre/versions/requirements_$VERSION_YEAR.txt
 fi
 
-#       run: (cut -f2,3 /cre/versions.txt > cre/versions/versions_2024.txt)
-#       run: (cp /cre/requirements.txt.orig cre/versions/requirements_2024.txt)
 
 
