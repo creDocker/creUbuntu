5062ec8b7e59174fd13646135746b6d8d54ace91
KMicha <michael.kahle@yahoo.de>
2026-03-24 14:16:08 +0100

    use uv for 2024

--- cre/init-python.sh
+++ cre/init-python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $(echo " $CRE_VERSION < 2026.0" | bc) -eq 1 ] ; then
+if [ $(echo " $CRE_VERSION < 2024.0" | bc) -eq 1 ] ; then
     /cre/init-python-old.sh
 else
     /cre/init-python-uv.sh
